974cf997ab98f21d304183229b994be8172de853
t-iffany <tiffanyxleong@gmail.com>
2022-10-31 01:08:10 +0000

    conditionals js modified

--- conditionals.js
+++ conditionals.js
@@ -40,3 +40,14 @@ if (temperature < -40 || temperature > 40) {
 if (!raining) {
   console.log("Leave your umbrella at home!");
 }
+
+//school activity
+const whichSchool = function (age) {
+  if (age < 13) {
+  console.log("Elementary School");
+} else if (age >= 13 && age <= 18) {
+  console.log("Secondary School");
+} else {
+  console.log("Lighthouse Labs");
+}
+}
