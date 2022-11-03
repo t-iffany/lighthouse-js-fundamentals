b6ca86193ffb25d948ed0bd961de4f0f0d01db44
t-iffany <tiffanyxleong@gmail.com>
2022-11-03 08:10:57 +0000

    Ice cream exercise

---
+++ ice-cream.js
@@ -0,0 +1,13 @@
+// Do not modify these first two lines
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+// Your code below here...
+iceCreamFlavours.push('root beer');
+console.log(iceCreamFlavours)
+//Get the first flavour
+console.log(iceCreamFlavours[0])
+//Get the Last Flavour
+console.log(iceCreamFlavours[iceCreamFlavours.length - 1])
+//Total # flavours
+console.log(iceCreamFlavours.length)
